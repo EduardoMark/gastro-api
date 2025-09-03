c51d9d3f3cf8f804692f79277a963eb36e299b83
Eduardo Marques <eduardomar1703@gmail.com>
2025-09-03 20:29:24 -0300

    feat: database config

---
+++ internal/database/db.go
@@ -0,0 +1,45 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/EduardoMark/gastro-api/internal/config"
+	"github.com/EduardoMark/gastro-api/internal/users"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func New(env *config.Env) (*gorm.DB, error) {
+	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		env.DbUser,
+		env.DbPassword,
+		env.DbHost,
+		env.DbPort,
+		env.DbName,
+	)
+
+	sqlDB, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open pgx connection: %v", err)
+	}
+
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		Conn: sqlDB,
+	}), &gorm.Config{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect database: %v", err)
+	}
+
+	if err := sqlDB.Ping(); err != nil {
+		return nil, fmt.Errorf("failed to ping database: %v", err)
+	}
+
+	return db, nil
+}
+
+func Migrate(db *gorm.DB) error {
+	return db.AutoMigrate(
+		users.User{},
+	)
+}
